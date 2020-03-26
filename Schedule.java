cfaa0f3b852d55ac14df1284624c1882420bd928
yugiohxl <38139912+yugiohxl@users.noreply.github.com>
2020-03-25 21:09:47 -0400

    Schedule class template. -SN

---
+++ Schedule.java
@@ -0,0 +1,9 @@
+public class Schedule{
+    //—————Fields—————
+    //      Classes: set <Class>
+    set<Class> classes;
+    //      Classrooms: set <Classrooms>
+    set<Classroom> classroomset;
+    //      Professor: set <Professors>
+    //      Students: set <Students>
+}
