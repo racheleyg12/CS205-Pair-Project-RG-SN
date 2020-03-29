9fbf083cd0480bffeed5116c59c995863bbe9366
yugiohxl <38139912+yugiohxl@users.noreply.github.com>
2020-03-28 21:23:54 -0400

    Schedule class template (get & find) methods. -SN

--- Schedule.java
+++ Schedule.java
@@ -11,4 +11,44 @@ public class Schedule{
     set<Student> students; //is this class/set still needed
 
 
+    ///methods:
+    //  addClass (Class c)
+    public void addClass(Class c){
+        classes.add(c);
+    }
+    //      list <Class> getClasses
+    public list<Class> getClasses() {
+        return classes;
+    }
+
+    //      Class findClass (string name)
+    public Class findClass(String name){
+
+    }
+    //      addClassroom (Classroom r)
+    public void addClassroom(Classroom r){
+        classroomset.add(r);
+    }
+    //      list <Classroom> getClasssrooms
+    public set<Classroom> getClassroomset() {
+        return classroomset;
+    }
+    //      Classroom findClassroom (string name)
+    public Classroom findClassroom(String name){
+
+    }
+    //      addProfessor (Professor p)
+    public void addProfessor(Professor p) {
+        professors.add(p);
+    }
+    //      list <Professor> getProfessors
+    public set<Professor> getProfessors() {
+        return professors;
+    }
+    //      Professor findProfessor(string name)
+    public Professor findProfessor(string name){
+
+    }
+
+
 }
