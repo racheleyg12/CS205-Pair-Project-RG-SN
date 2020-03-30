a4173f36af8f7caade3d11f07fb409cc87dec5eb
yugiohxl <38139912+yugiohxl@users.noreply.github.com>
2020-03-30 00:38:54 -0400

    Schedule class template methods. -SN

--- Schedule.java
+++ Schedule.java
@@ -20,10 +20,13 @@ public class Schedule{
     public list<Class> getClasses() {
         return classes;
     }
-
     //      Class findClass (string name)
     public Class findClass(String name){
-
+        for (Class c : classes) {
+            if(c.name == name){ //by name or dept?
+                return c;
+            } //what if there are multiples
+        }
     }
     //      addClassroom (Classroom r)
     public void addClassroom(Classroom r){
@@ -35,7 +38,11 @@ public class Schedule{
     }
     //      Classroom findClassroom (string name)
     public Classroom findClassroom(String name){
-
+        for (Classroom cr : classroomset) {
+            if(cr.name == name){ // checking by name (building + room)
+                return cr;
+            } //what if there are multiples
+        }
     }
     //      addProfessor (Professor p)
     public void addProfessor(Professor p) {
@@ -47,7 +54,28 @@ public class Schedule{
     }
     //      Professor findProfessor(string name)
     public Professor findProfessor(string name){
+        for (Professor p : professors) {
+            if(p.name == name){ //what if there are multiple porfessors of same name
+                return p;
+            }
+        }
+        // if no professors match, then nothing
+    }
 
+    //Schedule classes base on their time & availability of classrooms
+    //Optimally schedule classes - using least amount of classrooms
+    //Returns Optimally schedule of all classes & classrooms in the object
+    public list<string> scheduleClasses(){
+        list<string> sched = new list<string>;
+
+        return sched;
+    }
+
+    //Gets all classes occurring in classroom
+    list<Class> getClassesInClassroom(Classroom r){
+        list<Class> inRoom = new list<Class>();
+
+        return inRoom;
     }
 
 
