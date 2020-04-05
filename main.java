c52a6ff1000defa0253588976ec070a0d2da0730
racheleyg12 <rgoldma1@uvm.edu>
2020-04-05 15:06:56 -0400

    will run main in simulation

--- main.java
+++
@@ -1,9 +0,0 @@
-public class main {
-      
-  public static void main(String args[]) {
-    Library library;
-
-    library = Library.get();
-    Simulation simulation = new Simulation(library);
-  }
-}
