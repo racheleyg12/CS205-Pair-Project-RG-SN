34b4d220a1ae2760e9bf937e789efa7ae6c8d68f
racheleyg12 <rgoldma1@uvm.edu>
2020-04-04 20:13:56 -0400

    Working on running a functional simulation

--- Simulation.java
+++ Simulation.java
@@ -2,8 +2,13 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Simulation {
-
-
-
-
+  public Simulation(Schedule schedule) {
+        
+   
+    Professor bob = new Professor("Robert Erickson","CS","Innovation 304E","rerickso@uvm.edu");
+    double[] classTime = {9.5, 10.5};
+    schedule.addClass(new Class("CS", "CS008 Intro to Web Site Dev",bob , 20, new ArrayList<Class>(), classTime));
+    
+  
+   }
 }
